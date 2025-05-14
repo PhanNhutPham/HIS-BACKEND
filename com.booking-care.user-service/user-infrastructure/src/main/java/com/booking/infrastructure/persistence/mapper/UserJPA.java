c59001c9adbb2f8ae12441f729f7995b62a0a267
Phan Nhut Pham <pham.2174801030076@vanlanguni.vn>
2025-05-14 13:03:58 +0700

    lai ne

--- com.booking-care.user-service/user-infrastructure/src/main/java/com/booking/infrastructure/persistence/mapper/UserJPA.java
+++ com.booking-care.user-service/user-infrastructure/src/main/java/com/booking/infrastructure/persistence/mapper/UserJPA.java
@@ -3,9 +3,14 @@ package com.booking.infrastructure.persistence.mapper;
 
 import com.booking.domain.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserJPA  extends JpaRepository<User, String> {
     Optional<User> findUserByUserId(String userId);
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.nameRole = :roleName")
+    List<User> findUsersByRoleName(@Param("roleName") String roleName);
 }
